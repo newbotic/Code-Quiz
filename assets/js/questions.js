35f96a43a8e18bc234e868d93a659d9c0fd5ae42
newbotic <like_plus@yahoo.es>
2023-12-02 13:00:06 +0000

    logic update

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,5 +1,7 @@
 // var x = document.getElementById('start');
 // x.addEventListener('click', timerFunction);
+// add event listener
+
 x.addEventListener('click', quizFunction);
 
 function quizFunction() {
