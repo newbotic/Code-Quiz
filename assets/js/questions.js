8d859a12bd2e6bf8172b0c236e0621f2bcf3fc4b
newbotic <like_plus@yahoo.es>
2023-12-02 14:29:03 +0000

    add questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,4 @@
-// var x = document.getElementById('start');
+var x = document.getElementById('start');
 // x.addEventListener('click', timerFunction);
 // add event listener
 
@@ -7,3 +7,54 @@ x.addEventListener('click', quizFunction);
 function quizFunction() {
     alert('quiz start');
   }
+
+
+  var questions = [
+    {
+      question: "What is the correct way to declare a variable in JavaScript?",
+      answers: [
+        "var x = 5;",
+        "variable x = 5;",
+        "x = 5;",
+        "int x = 5;"
+      ],
+      correctAnswer: "var x = 5;"
+    },
+    {
+      question: "Which operator is used for strict equality in JavaScript?",
+      answers: ["==", "===", "=", "!="],
+      correctAnswer: "==="
+    },
+    {
+      question: "What does the 'DOM' stand for?",
+      answers: [
+        "Document Object Model",
+        "Data Object Model",
+        "Digital Object Model",
+        "Dynamic Object Model"
+      ],
+      correctAnswer: "Document Object Model"
+    },
+    {
+        question: "What does the 'NaN' in JavaScript stand for?",
+        answers: [
+          "Not a Node",
+          "No access Notification",
+          "Not a Number",
+          "Nonetheless a Null"
+        ],
+        correctAnswer: "Not a Number"
+      },
+      {
+        question: "How do you comment a single line of code in JavaScript?",
+        answers: [
+          "// This is a comment",
+          "' This is a comment",
+          "# This is a comment",
+          "/* This is a comment */"
+        ],
+        correctAnswer: "// This is a comment"
+      },
+  ];
+  
+  console.log(questions[0].answers);
