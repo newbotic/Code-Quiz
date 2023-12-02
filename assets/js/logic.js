7feb0ff57c14afa0d36f7a8a6daa2c704370811d
newbotic <like_plus@yahoo.es>
2023-12-02 12:50:51 +0000

    init commit

---
+++ assets/js/logic.js
@@ -0,0 +1,7 @@
+  var x = document.getElementById('start');
+  x.addEventListener('click', timerFunction);
+//   x.addEventListener('click', quizFunction);
+
+  function timerFunction() {
+    alert('timer start');
+  }
---
+++ assets/js/questions.js
@@ -0,0 +1,7 @@
+// var x = document.getElementById('start');
+// x.addEventListener('click', timerFunction);
+x.addEventListener('click', quizFunction);
+
+function quizFunction() {
+    alert('quiz start');
+  }
