aef944a4ce95052c25e3e1b75ae4ada9bade6b49
newbotic <like_plus@yahoo.es>
2023-12-03 13:52:57 +0000

    questions

--- assets/js/logic.js
+++
@@ -1,8 +0,0 @@
-  var x = document.getElementById('start');
-//  add event listener 
-  x.addEventListener('click', timerFunction);
-//   x.addEventListener('click', quizFunction);
-
-  function timerFunction() {
-    alert('timer start');
-  }
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,60 +1,54 @@
-var x = document.getElementById('start');
-// x.addEventListener('click', timerFunction);
+var startButton = document.getElementById("start");
+
 // add event listener
 
-x.addEventListener('click', quizFunction);
+startButton.addEventListener("click", function() {
+  startQuiz();
+});
 
-function quizFunction() {
-    alert('quiz start');
-  }
+// Array of Objects
 
+var questions = [
+  {
+    question: "What is the correct way to declare a variable in JavaScript?",
+    answers: ["var x = 5;", "variable x = 5;", "x = 5;", "int x = 5;"],
+    correctAnswer: "var x = 5;",
+  },
+  {
+    question: "Which operator is used for strict equality in JavaScript?",
+    answers: ["==", "===", "=", "!="],
+    correctAnswer: "===",
+  },
+  {
+    question: "What does the 'DOM' stand for?",
+    answers: [
+      "Document Object Model",
+      "Data Object Model",
+      "Digital Object Model",
+      "Dynamic Object Model",
+    ],
+    correctAnswer: "Document Object Model",
+  },
+  {
+    question: "What does the 'NaN' in JavaScript stand for?",
+    answers: [
+      "Not a Node",
+      "No access Notification",
+      "Not a Number",
+      "Nonetheless a Null",
+    ],
+    correctAnswer: "Not a Number",
+  },
+  {
+    question: "How do you comment a single line of code in JavaScript?",
+    answers: [
+      "// This is a comment",
+      "' This is a comment",
+      "# This is a comment",
+      "/* This is a comment */",
+    ],
+    correctAnswer: "// This is a comment",
+  },
+];
 
-  var questions = [
-    {
-      question: "What is the correct way to declare a variable in JavaScript?",
-      answers: [
-        "var x = 5;",
-        "variable x = 5;",
-        "x = 5;",
-        "int x = 5;"
-      ],
-      correctAnswer: "var x = 5;"
-    },
-    {
-      question: "Which operator is used for strict equality in JavaScript?",
-      answers: ["==", "===", "=", "!="],
-      correctAnswer: "==="
-    },
-    {
-      question: "What does the 'DOM' stand for?",
-      answers: [
-        "Document Object Model",
-        "Data Object Model",
-        "Digital Object Model",
-        "Dynamic Object Model"
-      ],
-      correctAnswer: "Document Object Model"
-    },
-    {
-        question: "What does the 'NaN' in JavaScript stand for?",
-        answers: [
-          "Not a Node",
-          "No access Notification",
-          "Not a Number",
-          "Nonetheless a Null"
-        ],
-        correctAnswer: "Not a Number"
-      },
-      {
-        question: "How do you comment a single line of code in JavaScript?",
-        answers: [
-          "// This is a comment",
-          "' This is a comment",
-          "# This is a comment",
-          "/* This is a comment */"
-        ],
-        correctAnswer: "// This is a comment"
-      },
-  ];
-  
-  console.log(questions[0].answers);
+//   console.log(questions[0].answers);
